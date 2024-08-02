60bffad9f896a31c884d482a916e74b0bd5a9055
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 17:44:46 +0300

    add a frame and a panel

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBounds(0,0,100,100);
+        panel.setBackground(Color.red);
+        panel.setLayout(null);
+
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.add(panel);
+        frame.setLayout(null);
+
+
     }
 }
