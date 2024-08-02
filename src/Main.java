ddccf709bcc86869eaa8c7461b55e108bf641b56
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 21:51:06 +0300

    add frame,panel,button and label

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,44 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        JPanel redPanel = new JPanel();
+        JPanel greenPanel = new JPanel();
+        JPanel bluePanel = new JPanel();
+        JButton greenButton = new JButton();
+        JLabel label = new JLabel();
 
-        panel.setBounds(0,0,100,100);
-        panel.setBackground(Color.red);
-        panel.setLayout(null);
+        label.setText("Java genius");
+        label.setBounds(400,70,100,100);
+        label.setFont(new Font("Courier New",Font.BOLD,10));
+        label.setOpaque(true);
+        label.setBackground(Color.BLUE);
 
+        greenButton.setBounds(330,50,100,50);
+        greenButton.setText("Submit");
+        greenButton.setFocusable(false);
+        greenButton.addActionListener(e -> System.out.println("Hey, man"));
+
+        redPanel.setBounds(0,0,100,100);
+        redPanel.setBackground(Color.red);
+        redPanel.setLayout(null);
+
+
+        greenPanel.setBounds(110,0,100,100);
+        greenPanel.setBackground(Color.green);
+
+
+        bluePanel.setBounds(220,0,100,100);
+        bluePanel.setBackground(Color.blue);
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setSize(500,500);
-        frame.add(panel);
+        frame.add(redPanel);
+        frame.add(greenPanel);
+        frame.add(bluePanel);
+        frame.add(greenButton);
+        frame.add(label);
         frame.setLayout(null);
 
 
